4e51ac30cdb44d40e9f17764809614618bbf0ac7
Calvin Belden <cbelden@nd.edu>
2014-04-29 23:49:52 -0400

    Check if log path exists in device.

--- pybeaconclient/client.py
+++ pybeaconclient/client.py
@@ -1,5 +1,6 @@
 import paramiko
 import os
+import errno
 
 
 class PyBeaconClient():
@@ -20,58 +21,73 @@ class PyBeaconClient():
             print 'Check if connected to network correctly.'
             exit(-1)
 
+    def _devicepath_exists(self, path):
+        """Ensures the specified device path exists."""
+
+        sftp = self._ssh.open_sftp()
+
+        try:
+            sftp.state(path)
+        except IOError, e:
+            if e.args[0] == errno.ENOENT:
+                print "Device path does not exist"
+                raise e
+
+        sftp.close()
+
+    def _create_log_folders(self, localpath, logfolders):
+        """Generates a local log directory in localpath for each directory in logfolders."""
+
+        for logfolder in logfolders:
+            path = '/'.join([localpath, logfolder])
+
+            try:
+                os.makedirs(path)
+            except OSError, e:
+                # If directory exists, continue to next folder
+                if e.args[0] == 17:
+                    continue
+                raise e
+
     def get_logs(self, devicepath, localpath):
         """Retrieves the beacon logs stored on the logging device at devicepath and stores them locally in localpath."""
 
-        # Format directories
+        # Format paths
         devicepath = devicepath.rstrip('/')
         localpath = localpath.rstrip('/')
+
+        # Ensure devicepath exists
+        self._devicepath_exists(devicepath)
 
         # Get log directories (one for each day EXCEPT most recent)
         stdin, stdout, stderr = self._ssh.exec_command('ls ' + devicepath)
         logfolders = [d.rstrip() for d in stdout.readlines()[:-1]]
-
-        # Print no. found folders
         print 'Found ', len(logfolders), ' to retrieve..'
 
         # Create local log folders
         self._create_log_folders(localpath, logfolders)
 
-        # Open sftp session
-        ftp = self._ssh.open_sftp()
-
         # Copy beacon logs to local machine
+        sftp = self._ssh.open_sftp()
+
         for logfolder in logfolders:
 
             fullpath = '/'.join([devicepath, logfolder])
             stdin, stdout, stderr = self._ssh.exec_command('ls ' + fullpath)
             logs = [l.rstrip() for l in stdout.readlines()]
 
-            print 'Copying ' + logfolder + ' ...'
+            print 'Retrieving log folder: ' + logfolder + ' ...'
 
             for log in logs:
 
                 src = '/'.join([devicepath, logfolder, log])
                 target = '/'.join([localpath, logfolder, log])
 
-                print '\tfetching ' + log + '...'
+                print '\tfetching file: ' + log + '...'
 
                 try:
-                    ftp.get(src, target)
+                    sftp.get(src, target)
                 except Exception:
                     print '*** Error copying ' + src + ' to ' + target
 
-        ftp.close()
-
-    def _create_log_folders(self, localpath, logfolders):
-        """Generates a local log directory in localpath for each directory in logfolders."""
-
-        for logfolder in logfolders:
-
-            path = '/'.join([localpath, logfolder])
-
-            try:
-                os.makedirs(path)
-
-            except OSError:
-                print 'Error creating directory: ' + path
+        sftp.close()
