05687356e97cf3920c005088b6f9ea032301d36b
Calvin Belden <cbelden@nd.edu>
2014-05-01 11:32:56 -0400

    Refactored log downloading. Added set_utc_time method. Stubbed out delete_logs method.

--- pybeaconclient/client.py
+++ pybeaconclient/client.py
@@ -1,6 +1,7 @@
 import paramiko
 import os
 import errno
+from datetime import datetime
 
 
 class PyBeaconClient():
@@ -21,13 +22,73 @@ class PyBeaconClient():
             print 'Check if connected to network correctly.'
             exit(-1)
 
+    def download_logs(self, devicepath, localpath, delete=False):
+        """Retrieves the beacon logs stored on the logging device at devicepath and stores them locally in localpath."""
+
+        # Format paths
+        devicepath = devicepath.rstrip('/')
+        localpath = localpath.rstrip('/')
+
+        # Ensure devicepath exists
+        self._devicepath_exists(devicepath)
+
+        # Get log directories (one for each day EXCEPT most recent)
+        stdin, stdout, stderr = self._ssh.exec_command('ls ' + devicepath)
+        logfolders = [d.rstrip() for d in stdout.readlines()[:-1]]
+        print 'Found ', len(logfolders), ' to retrieve..'
+
+        # Create local log folders
+        self._create_log_folders(localpath, logfolders)
+
+        # Download each subdirectory in the main log directory
+        for logfolder in logfolders:
+            print 'Retrieving log folder: ' + logfolder + ' ...'
+            self._download_log_folder(devicepath, logfolder, localpath)
+
+    def set_utc_date(self):
+        """Sets the device's utc date to the local utc date."""
+
+        utc_date = str(datetime.utcnow())
+
+        # Strip millisecond data
+        utc_date = utc_date.split('.')[0]
+
+        stdin, stdout, stderr = self._ssh.exec_command('sudo date -s "' + utc_date + '" -u')
+
+    def delete_logs(self, devicepath, logdirs):
+        """Deletes all of the specified log directories."""
+        pass
+
+    def _download_log_folder(self, devicepath, log, localpath):
+        """Stores log .txt files in logpath in the directory specified by localpath."""
+
+        logpath = '/'.join([devicepath, log])
+        stdin, stdout, stderr = self._ssh.exec_command('ls ' + logpath)
+        log_files = [l.rstrip() for l in stdout.readlines()]
+
+        # Open sftp connection
+        sftp = self._ssh.open_sftp()
+
+        for f in log_files:
+            src = '/'.join([logpath, f])
+            target = '/'.join([localpath, log, f])
+
+            print '\tfetching file: ' + f + '...'
+
+            try:
+                sftp.get(src, target)
+            except Exception:
+                print '*** Error copying ' + src + ' to ' + target
+
+        sftp.close()
+
     def _devicepath_exists(self, path):
         """Ensures the specified device path exists."""
 
         sftp = self._ssh.open_sftp()
 
         try:
-            sftp.state(path)
+            sftp.stat(path)
         except IOError, e:
             if e.args[0] == errno.ENOENT:
                 print "Device path does not exist"
@@ -48,46 +109,3 @@ class PyBeaconClient():
                 if e.args[0] == 17:
                     continue
                 raise e
-
-    def get_logs(self, devicepath, localpath):
-        """Retrieves the beacon logs stored on the logging device at devicepath and stores them locally in localpath."""
-
-        # Format paths
-        devicepath = devicepath.rstrip('/')
-        localpath = localpath.rstrip('/')
-
-        # Ensure devicepath exists
-        self._devicepath_exists(devicepath)
-
-        # Get log directories (one for each day EXCEPT most recent)
-        stdin, stdout, stderr = self._ssh.exec_command('ls ' + devicepath)
-        logfolders = [d.rstrip() for d in stdout.readlines()[:-1]]
-        print 'Found ', len(logfolders), ' to retrieve..'
-
-        # Create local log folders
-        self._create_log_folders(localpath, logfolders)
-
-        # Copy beacon logs to local machine
-        sftp = self._ssh.open_sftp()
-
-        for logfolder in logfolders:
-
-            fullpath = '/'.join([devicepath, logfolder])
-            stdin, stdout, stderr = self._ssh.exec_command('ls ' + fullpath)
-            logs = [l.rstrip() for l in stdout.readlines()]
-
-            print 'Retrieving log folder: ' + logfolder + ' ...'
-
-            for log in logs:
-
-                src = '/'.join([devicepath, logfolder, log])
-                target = '/'.join([localpath, logfolder, log])
-
-                print '\tfetching file: ' + log + '...'
-
-                try:
-                    sftp.get(src, target)
-                except Exception:
-                    print '*** Error copying ' + src + ' to ' + target
-
-        sftp.close()
